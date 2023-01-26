9379112c71173d6270332731b5792edffd37f640
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-01-26 21:52:14 +1100

    wip: add select and assertion to test

--- mocksnow/__init__.py
+++ mocksnow/__init__.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from contextlib import contextmanager
 from typing import Any, Iterator
-from unittest.mock import Mock, create_autospec, patch
+from unittest.mock import create_autospec, patch
 
 import snowflake.connector
 
@@ -18,12 +18,14 @@ def mock_execute(
 def mock() -> Iterator[None]:
     with patch.object(snowflake.connector, "connect", autospec=True) as connect:
         mock_cursor = create_autospec(snowflake.connector.cursor.SnowflakeCursor)
+        mock_cursor.execute.side_effect = mock_execute
+
+
         mock_connection = create_autospec(snowflake.connector.SnowflakeConnection)
         mock_connection.cursor.return_value = mock_cursor
-        mock_cursor.execute.side_effect = mock_execute
         connect.return_value = mock_connection
 
-        # when used as a context manager return itself
+        # allow mocks to be used as context managers
         mock_connection.__enter__.return_value = mock_connection
         mock_cursor.__enter__.return_value = mock_cursor
 
--- tests/test_example.py
+++ tests/test_example.py
@@ -11,6 +11,10 @@ def test_example(conn: snowflake.connector.SnowflakeConnection):
     with conn.cursor() as cur:
         cur.execute("create schema if not exists jaffles")
         cur.execute("drop table if exists customers")
-        cur.execute("create table jaffles (ID int, FIRST_NAME varchar, LAST_NAME varchar)")
-        cur.execute("insert into jaffles values (1, 'Jenny', 'P')")
-        cur.execute("insert into jaffles values (2, 'Jasper', 'M')")
+        cur.execute("create table customers (ID int, FIRST_NAME varchar, LAST_NAME varchar)")
+        cur.execute("insert into customers values (1, 'Jenny', 'P')")
+        cur.execute("insert into customers values (2, 'Jasper', 'M')")
+        cur.execute("select id, first_name, last_name from customers")
+        result = cur.fetchall()
+
+        assert result == [(1, "Jenny", "P"), (2, "Jasper", "M")]
