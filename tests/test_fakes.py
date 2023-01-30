5ba686d7e88c689c05a79c82a89b9e6b145db9a6
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-01-30 16:45:52 +1100

    build: add coverage for using multiple connections

--- tests/test_fakes.py
+++ tests/test_fakes.py
@@ -23,21 +23,18 @@ def test_fetchone(conn: snowflake.connector.SnowflakeConnection):
 
         assert result == (1, "Jenny", "P")
 
+def test_connect_with_database_and_schema(conn: snowflake.connector.SnowflakeConnection):
+    # connect without default database and schema
+    with snowflake.connector.connect() as conn1:
+        with conn1.cursor() as cur:
+            # use the table's fully qualified name
+            cur.execute("create schema marts.jaffles")
+            cur.execute("create table marts.jaffles.customers (ID int, FIRST_NAME varchar, LAST_NAME varchar)")
+            cur.execute("insert into marts.jaffles.customers values (1, 'Jenny', 'P')")
 
-def test_qualified_with_database(conn: snowflake.connector.SnowflakeConnection):
-    with conn.cursor() as cur:
-        cur.execute("create schema marts.jaffles")
-        cur.execute("create table marts.jaffles.customers (ID int, FIRST_NAME varchar, LAST_NAME varchar)")
-
-
-def test_uses_connection_database_and_schema(conn: snowflake.connector.SnowflakeConnection):
-    with conn.cursor() as cur:
-        cur.execute("create schema marts.jaffles")
-
-    with snowflake.connector.connect(database="marts", schema="jaffles") as conn:
-        with conn.cursor() as cur:
-            cur.execute("create table customers (ID int, FIRST_NAME varchar, LAST_NAME varchar)")
-            cur.execute("insert into customers values (1, 'Jenny', 'P')")
+    # in a separate connection, connect using the database and schema from above
+    with snowflake.connector.connect(database="marts", schema="jaffles") as conn2:
+        with conn2.cursor() as cur:
             cur.execute("insert into customers values (2, 'Jasper', 'M')")
             cur.execute("select id, first_name, last_name from marts.jaffles.customers")
             result = cur.fetchall()
