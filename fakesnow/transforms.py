28d06ee6fed79f513d28659a35e541ef8dae09fd
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2023-02-27 20:28:28 +1100

    chore: ugrpade to latest sqlglot
    which now treats database name in CREATE DATABASE as an expression which preserves case.
    see https://github.com/tobymao/sqlglot/issues/1229

--- fakesnow/transforms.py
+++ fakesnow/transforms.py
@@ -93,7 +93,9 @@ def create_database(expression: exp.Expression) -> exp.Expression:
     """
 
     def transform_create_db(node: exp.Command) -> exp.Command:
-        if match := re.search("create database (\\w+)", cast(str, node.this), flags=re.IGNORECASE):
+        if isinstance(node.expression, str) and (
+            match := re.search("database (\\w+)", cast(str, node.expression), flags=re.IGNORECASE)
+        ):
             db_name = match[1]
             return exp.Command(
                 this="ATTACH", expression=exp.Literal(this=f"DATABASE ':memory:' AS {db_name}", is_string=True)
