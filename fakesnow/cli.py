83254d30a2c5b3405c8d7a4c7167873ed20d6391
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2024-01-28 21:17:44 +1100

    fix: cli passes -m and -d to target

--- fakesnow/cli.py
+++ fakesnow/cli.py
@@ -5,8 +5,6 @@ from collections.abc import Sequence
 
 import fakesnow
 
-USAGE = "Usage: fakesnow <path> | -m <module> [<arg>]..."
-
 
 def arg_parser() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
@@ -19,28 +17,50 @@ def arg_parser() -> argparse.ArgumentParser:
         help="databases path. Use existing database files from this path or create them here if they don't already "
         "exist. If None databases are in-memory.",
     )
-    parser.add_argument("-m", "--module", help="module")
-    parser.add_argument("path", type=str, nargs="?", help="path")
-    parser.add_argument("args", nargs="*", help="args")
+    parser.add_argument("-m", "--module", help="target module")
+    parser.add_argument("path", type=str, nargs="?", help="target path")
+    parser.add_argument("targs", nargs="*", help="target args")
     return parser
+
+
+def split(args: Sequence[str]) -> tuple[Sequence[str], Sequence[str]]:
+    # split the arguments into two lists either:
+    # 1) after the first -m flag, or
+    # 2) after the first positional arg
+    in_flag = False
+    i = 0
+    for i in range(len(args)):
+        a = args[i]
+        if a in ["-m", "--module"]:
+            i = min(i + 1, len(args) - 1)
+            break
+        elif a.startswith("-"):
+            in_flag = True
+        elif not in_flag:
+            break
+        else:
+            in_flag = False
+
+    return args[: i + 1], args[i + 1 :]
 
 
 def main(args: Sequence[str] = sys.argv[1:]) -> int:
     parser = arg_parser()
-    pargs, remainder = parser.parse_known_args(args)
+    # split args so the fakesnow cli doesn't consume from the target's args (eg: -m and -d)
+    fsargs, targs = split(args)
+    pargs = parser.parse_args(fsargs)
 
     with fakesnow.patch(db_path=pargs.db_path):
         if module := pargs.module:
-            if pargs.path:
-                sys.argv = [module, pargs.path, *pargs.args, *remainder]
-            else:
-                sys.argv = [module]
+            # NB: pargs.path and pargs.args are consumed by targs
+            sys.argv = [module, *targs]
 
             # add current directory to path to mimic python -m
             sys.path.insert(0, "")
             runpy.run_module(module, run_name="__main__", alter_sys=True)
         elif path := pargs.path:
-            sys.argv = [path, *pargs.args, *remainder]
+            # NB: pargs.args is consumed by targs
+            sys.argv = [path, *targs]
 
             runpy.run_path(path, run_name="__main__")
         else:
--- tests/test_cli.py
+++ tests/test_cli.py
@@ -3,16 +3,27 @@ from pytest import CaptureFixture
 import fakesnow.cli
 
 
+def test_split() -> None:
+    assert fakesnow.cli.split(["pytest", "-m", "integration"]) == (["pytest"], ["-m", "integration"])
+    assert fakesnow.cli.split(["-m", "pytest", "-m", "integration"]) == (["-m", "pytest"], ["-m", "integration"])
+    assert fakesnow.cli.split(["pytest"]) == (["pytest"], [])
+    assert fakesnow.cli.split(["-m", "pytest"]) == (["-m", "pytest"], [])
+    assert fakesnow.cli.split(["-d", "databases/", "--module", "pytest", "-m", "integration"]) == (
+        ["-d", "databases/", "--module", "pytest"],
+        ["-m", "integration"],
+    )
+
+
 def test_run_module(capsys: CaptureFixture) -> None:
     fakesnow.cli.main(["-m", "tests.hello"])
 
     captured = capsys.readouterr()
     assert captured.out == "('Hello fake world!',)\n"
 
-    fakesnow.cli.main(["-m", "tests.hello", "frobnitz", "--colour", "rainbow"])
+    fakesnow.cli.main(["-m", "tests.hello", "frobnitz", "--colour", "rainbow", "-m", "integration"])
 
     captured = capsys.readouterr()
-    assert captured.out == "('Hello fake frobnitz --colour rainbow!',)\n"
+    assert captured.out == "('Hello fake frobnitz --colour rainbow -m integration!',)\n"
 
 
 def test_run_path(capsys: CaptureFixture) -> None:
@@ -21,10 +32,10 @@ def test_run_path(capsys: CaptureFixture) -> None:
     captured = capsys.readouterr()
     assert captured.out == "('Hello fake world!',)\n"
 
-    fakesnow.cli.main(["tests/hello.py", "frobnitz", "--colour", "rainbow"])
+    fakesnow.cli.main(["tests/hello.py", "frobnitz", "--colour", "rainbow", "-m", "integration"])
 
     captured = capsys.readouterr()
-    assert captured.out == "('Hello fake frobnitz --colour rainbow!',)\n"
+    assert captured.out == "('Hello fake frobnitz --colour rainbow -m integration!',)\n"
 
 
 def test_run_no_args_shows_usage(capsys: CaptureFixture) -> None:
