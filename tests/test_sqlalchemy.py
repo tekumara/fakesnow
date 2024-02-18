4b122a8e5e38c5f80fb8a17ccb6b3dd15b97ee0b
DanCardin <ddcardin@gmail.com>
2024-02-18 16:32:48 -0500

    fix: Create test for sqlalchemy table reflection.

--- tests/test_sqlalchemy.py
+++ tests/test_sqlalchemy.py
@@ -1,3 +1,6 @@
+import duckdb
+import pytest
+from sqlalchemy import Column, MetaData, Table, types
 from sqlalchemy.engine import Engine
 from sqlalchemy.sql.expression import TextClause
 
@@ -10,3 +13,27 @@ def test_create_view(snowflake_engine: Engine):
         result = conn.execute("SELECT database_name FROM foo")
         assert result
         assert result.fetchall() == [("DB1",)]
+
+
+def test_metadata_createall(snowflake_engine: Engine):
+    metadata = MetaData()
+
+    table = Table("foo", metadata, Column(types.Integer, name="id"), Column(types.String, name="name"))
+    metadata.create_all(bind=snowflake_engine)
+
+    with snowflake_engine.connect() as conn:
+        result = conn.execute(table.select())
+        assert result
+        assert result.fetchall() == []
+
+
+@pytest.mark.xfail(
+    reason="sqlglot currently has unsupported SHOW variants",
+    strict=True,
+    raises=duckdb.ParserException,
+)
+def test_reflect(snowflake_engine: Engine):
+    snowflake_engine.execute(TextClause("CREATE TABLE foo (id INTEGER, name VARCHAR)"))
+
+    metadata = MetaData()
+    metadata.reflect(bind=snowflake_engine, only=["foo"])
