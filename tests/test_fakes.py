29625f3966324ed4a39de5ada85f539728ec91df
Selman Kayrancioglu <selmankayrancioglu@gmail.com>
2024-04-02 15:22:19 +0300

    fix: fix GET_PATH precedence for JSONExtractScalar

--- tests/test_fakes.py
+++ tests/test_fakes.py
@@ -792,6 +792,15 @@ def test_get_path_precedence(cur: snowflake.connector.cursor.SnowflakeCursor):
     cur.execute("select {'K1': {'K2': 1}} as col where col:K1:K2 > 0")
     assert indent(cur.fetchall()) == [('{\n  "K1": {\n    "K2": 1\n  }\n}',)]
 
+    cur.execute("select {'K1': {'K2': 1}} as col where col:K1:K2 > 0")
+    assert indent(cur.fetchall()) == [('{\n  "K1": {\n    "K2": 1\n  }\n}',)]
+
+    cur.execute("create table example (col variant)")
+
+    cur.execute("""insert into example values ('{"K1": "a", "K2": "b"}')""")
+    cur.execute("select case when col:K1::VARCHAR = 'a' and col:K2::VARCHAR = 'b' then 'yes' end from example")
+    assert cur.fetchall() == [("yes",)]
+
 
 def test_get_result_batches(cur: snowflake.connector.cursor.SnowflakeCursor):
     # no result set
