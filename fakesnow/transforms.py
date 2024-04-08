fcb0927899b6cec8f64a5bf120037c748195f4fd
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2024-04-08 14:31:22 +1000

    simplify

--- fakesnow/transforms.py
+++ fakesnow/transforms.py
@@ -200,23 +200,24 @@ def datediff_string_literal_timestamp_cast(expression: exp.Expression) -> exp.Ex
     if not isinstance(expression, exp.DateDiff):
         return expression
 
-    datediff = expression
-    op1 = datediff.this.copy()
-    op2 = datediff.expression.copy()
+    op1 = expression.this.copy()
+    op2 = expression.expression.copy()
 
     if isinstance(op1, exp.Literal) and op1.is_string:
         op1 = exp.Cast(
             this=op1,
+            # TODO: support TIMESTAMP_TYPE_MAPPING of TIMESTAMP_LTZ/TZ
             to=exp.DataType(this=exp.DataType.Type.TIMESTAMP, nested=False, prefix=False),
         )
 
     if isinstance(op2, exp.Literal) and op2.is_string:
         op2 = exp.Cast(
             this=op2,
+            # TODO: support TIMESTAMP_TYPE_MAPPING of TIMESTAMP_LTZ/TZ
             to=exp.DataType(this=exp.DataType.Type.TIMESTAMP, nested=False, prefix=False),
         )
 
-    new_datediff = datediff.copy()
+    new_datediff = expression.copy()
     new_datediff.set("this", op1)
     new_datediff.set("expression", op2)
 
