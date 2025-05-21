7501dd074891d8db2d1c7290474aa2457b42e1c5
Rosset Denis <denis.rosset@romande-energie.ch>
2025-05-21 15:14:15 +0200

    Failing test

--- tests/test_fakes.py
+++ tests/test_fakes.py
@@ -126,6 +126,15 @@ def test_clone(cur: snowflake.connector.cursor.SnowflakeCursor):
     assert cur.fetchall() == [(1, "Jenny", True)]
 
 
+def test_convert_identifier_dollar_character(cur: snowflake.connector.cursor.SnowflakeCursor) -> None:
+    cur.execute("CREATE DATABASE ORG$INTERNAL;")
+    cur.execute("CREATE SCHEMA ORG$INTERNAL.SCHEMA;")
+    cur.execute("CREATE TABLE ORG$INTERNAL.SCHEMA.TABLE (amount NUMBER);")
+    cur.execute("INSERT INTO ORG$INTERNAL.SCHEMA.TABLE VALUES(1);")
+    cur.execute("SELECT * FROM ORG$INTERNAL.SCHEMA.TABLE;")
+    assert cur.fetchall() == [(1,)]
+
+
 def test_create_database_respects_if_not_exists() -> None:
     with tempfile.TemporaryDirectory(prefix="fakesnow-test") as db_path, fakesnow.patch(db_path=db_path):
         cursor = snowflake.connector.connect().cursor()
